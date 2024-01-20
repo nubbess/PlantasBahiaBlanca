6ee0867560bb3ca72f12623c2233c7706d398457
Julian <julianms999@gmail.com>
2024-01-20 02:22:42 -0300

    feat Web asp-webapi app for api controllers

---
+++ Web/Controllers/UsuariosController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateUser(Usuario usuario)
+        {
+            return Ok(usuario);
+        }
+    }
+}
